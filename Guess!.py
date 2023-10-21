8b92f972f722a1a05c295189877cd3ba5acb4fb0
Owen Orcan <144565916+OwenOrcan@users.noreply.github.com>
2023-10-21 00:44:46 -0400

    Update!
    -Changed Text Color
    -The Terminal or CMD does not automatically close when the loop ends
    -Minor String Changes

--- Guess!.py
+++ Guess!.py
@@ -1,32 +1,37 @@
+import os
 import random
+
+os.system('color 2')
+
+playing = True
+
+
 
 cards = ["ace of spades","king of spades","queen of spades", "jack of spades"]
 cards2 = ["ace of hearts","king of hearts","queen of hearts", "jack of hearts"]
-print("                GUESSING GAME!                        ")
-while True:
+print("                                    GUESS!                      ")
+while playing:
     try:
         balance = int(input("Deposit Balance: "))
-        if type(balance) == float:
-            print("Please Deposit Full Amounts.")
         if balance <= 0:
-            print("Balance Cannot Be Negative or 0")
+            print("Cannot Deposit Amounts That Are Less Than 0.")
         elif balance > 1000:
             print("Maximum Deposit Amount Is $1000.")
         else:
             break
     except Exception:
-        print("Please Check Your Money And Try Again.")
+        print("Could Not Complete Request, Maybe Check Your Bank Account Again?")
         continue
 
-while True:
+while playing:
     if balance == 0:
-        print("You Do Not Have Any Money.")
+        input("You Do Not Have Any Money Left, Press Enter To Quit.....")
         break
     try:
-        choose_deck = random.choice(["card1", "card2"])
-        if choose_deck == "card1":
+        choose_deck = random.randint(1,2)
+        if choose_deck == 1:
             hand = cards
-        elif choose_deck == "card2":
+        elif choose_deck == 2:
             hand = cards2
         print("Dealers Hand:\n", hand)
         print(f"Current Balance: ${balance}")
@@ -40,31 +45,33 @@ while True:
             print("!!!!!!!!!!!!!!!!Unsufficent Funds!!!!!!!!!!!!!!!!!!!!!")
             continue
         if bet < 0:
-            print("Cannot Bet 0!")
+            print("Cannot Bet $0!")
             continue
-        guess = input("Guess The Card: ").lower()
+        guess = input("Guess The Card: ").lower().strip()
         if guess == random_card:
             print("CORRECT!")
             balance = balance - bet
             balance = balance + (bet*2)
-            print(f"Your balance is ${balance}")
-
-            user_continue_choice = str(input("Do you want to continue? y/n ")).lower()
+            print(f"Your balance is now ${balance}")
+            user_continue_choice = str(input("Do you want to continue? y/n ")).lower().strip()
             if user_continue_choice == "y":
                 continue
             elif user_continue_choice == "n":
                 print("See You!")
+                input("Press Enter To Quit.....")
                 break
             else:
                 print("Could Not Identify Response, Quitting.")
+                input("Press Enter To Quit.....")
                 break
         else:
-            balance = 0
+            balance = balance - bet
             print(f"Sorry, the card was {random_card.upper()}")
-            break
+            input("Press Enter To Continue.....")
+            continue
 
     except EOFError:
-        print("Goodbye!")
+        input("Press Enter To Quit.")
         break
 
 
